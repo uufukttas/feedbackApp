03574937a008267631f5f6e27e5bdb9c768a7fc0
uufukttas <uufukttas@hotmail.com>
2022-02-02 00:23:10 +0300

    Feedback delete function updated.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import FeedbackData from './data/FeedbackData'
 
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete this item?')) {
+      setFeedback(feedback.filter(item => item.id !== id ));
+    }
+  };
+
   return (
     <>
       <Header/>
-      <FeedbackList feedback={feedback}/>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </>
   )
 }
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import { FaTimes } from 'react-icons/fa'
 
-function FeedbackItem({ item }) {
-    const handleClick = (id) => {
-        console.log(id)
-    }
+function FeedbackItem({ item, handleDelete }) {
+
   return (
     <div className="card">
         <div className="num-display">{item.rating}</div>
-        <button className='close' onClick={() => { handleClick(item.id)}}><FaTimes color='purple'/></button>
+        <button className='close' onClick={() => handleDelete(item.id)}><FaTimes color='purple'/></button>
         <div className="text-display">{item.text}</div>
     </div>
   )
