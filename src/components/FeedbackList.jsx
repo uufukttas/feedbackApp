e8a2d916793de83f3c97d82109491857d608b8c5
uufukttas <uufukttas@hotmail.com>
2022-02-03 23:06:45 +0300

    Fade In/Out Animateion functionality added.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,28 +1,52 @@
-import PropTypes from 'prop-types';
-import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from "framer-motion";
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>;
-  
+
   return (
     <div className="feedback-list">
-        {
-            feedback.map(item => {
-                return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-            })
-        }
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
+    </div>
+  );
+
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => {
+        return (
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        );
+      })}
     </div>
   );
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
-        })
-    ),
-}
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
