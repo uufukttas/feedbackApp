2c565b15318815d0a03b38946d4b806b3dd6fd13
uufukttas <uufukttas@hotmail.com>
2022-02-07 23:11:51 +0300

    Spinner Component added.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,12 +2,13 @@ import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>;
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) return <p>No feedback yet.</p>;
 
-  return (
+  return isLoading ? <Spinner/> : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => {
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,27 @@
 import { v4 as uuidv4 } from "uuid";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: uuidv4(),
-      rating: 10,
-      text: "This is feedback item 1",
-    },
-    {
-      id: uuidv4(),
-      rating: 9,
-      text: "This is feedback item 2",
-    },
-    {
-      id: uuidv4(),
-      rating: 8,
-      text: "This is feedback item 3",
-    },
-  ]);
-
+  const [feedback, setFeedback] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    getFeedback();
+  }, []);
+
+  const getFeedback = async () => {
+    const response = await fetch('http://localhost:5000/feedback?sort=id&_order=desc');
+    const data = await response.json();
+
+    setFeedback(data)
+    setIsLoading(false);
+  }
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -58,6 +54,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
